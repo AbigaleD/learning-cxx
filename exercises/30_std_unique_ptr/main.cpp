02c00b4c4ea0cbe0df156d96bda795211b0f834c
AbigaleD <abigale.dong@outlook.com>
2025-01-05 16:24:03 +0800

    fix wrong value again

--- exercises/30_std_unique_ptr/main.cpp
+++ exercises/30_std_unique_ptr/main.cpp
@@ -66,7 +66,7 @@ int main(int argc, char **argv) {
         ASSERT(problems[i].size() == answers[i].size(), "wrong size");
         for (auto j = 0; j < problems[i].size(); ++j) {
             // std::cout<<problems[i][j].c_str()<<" ";
-            ASSERT(std::strcmp(problems[i][j].c_str(), answers[i][j]) == 0, "wrong location");
+            ASSERT(strcmp(problems[i][j].c_str(), answers[i][j]) == 0, "wrong location");
         }
         // std::cout<<std::endl;
     }
